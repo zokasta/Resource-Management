5fe732108b975390ee223ecfdba371c231f8f50c
Zokasta <faizrajput1510@outlook.com>
2023-09-21 13:44:13 +0530

    commited

--- react-app/src/Conponents/buttonBox.js
+++ react-app/src/Conponents/buttonBox.js
@@ -4,7 +4,7 @@ function buttonBox(props)
         <button 
             type={props.type} 
             name={props.name} 
-            className="p-2 px-4 bg-green-400 w-min my-3 rounded-lg mb-3 text-white font-mono hover:bg-green-600">{props.title}</button>
+            className="p-2 px-4 bg-green-500 w-min m-3 rounded-lg mb-3 text-white font-mono hover:bg-green-600 hover:shadow-lg">{props.title}</button>
     );
 }
 export default buttonBox;
